7ed635a740aa0f3af36b5157d491ee1ee2e415e7
TempTeacher <manetsm1997@gmail.com>
2022-01-13 16:59:09 +0300

    add lesson material

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder = new StringBuilder("initial");
+
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer = new StringBuffer("initial");
+
+        String str = "cafe with \"Cafe name\" \n this is new line with \\";
+        System.out.println(str);
+
+        Pattern pattern = Pattern.compile("cafe", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            System.out.println(str.substring(matcher.start(), matcher.end()));
+        }
     }
 }
