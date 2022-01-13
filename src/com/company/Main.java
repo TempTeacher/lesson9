5a9a65aee652f2219f81072e932c0e554c095602
TempTeacher <manetsm1997@gmail.com>
2022-01-13 23:13:22 +0300

    from class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder = new StringBuilder("initial");
+//        stringBuilder = new StringBuilder("initial");
+        stringBuilder.append(1);
+        stringBuilder.append(" ");
+        stringBuilder.append(new ImmutableClass(new int[]{1}));
+        stringBuilder.delete(2, 5);
+        System.out.println(stringBuilder.toString());
+        stringBuilder = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            stringBuilder.append(i);
+        }
+        stringBuilder.deleteCharAt(2);
+//        stringBuilder.insert(2, "some input");
+        stringBuilder.insert(0, -1);
+        stringBuilder.replace(3, 6, " ");
+//        stringBuilder.reverse();
+        System.out.println(stringBuilder.toString());
 
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer = new StringBuffer("initial");
+        char c = '\n';
 
         String str = "cafe with \"Cafe name\" \n this is new line with \\";
         System.out.println(str);
